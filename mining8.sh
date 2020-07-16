d415593a0a54a7c630710a52c76cf3c78d2216f6
Dedi Sutardi <51874109+dedi-sutardi@users.noreply.github.com>
2020-07-16 15:59:52 +0700

    Create mining8.sh

---
+++ mining8.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Original script by Syarian
+# HAYA MINTA DI HARGAIN ORIGINAL JANGAN DI GANTI
+# Donasi tidak di paksa kan Yang mau Berdonasi silahkan
+# Doge : D8ccMV5NvDe3R6LsiGhUHtqMf8fRG7kAJh
+# ==================================================
+
+# Donwload File
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+
+# extrak file
+tar xf cpuminer-opt-linux.tar.gz 
+
+# Run Mining
+while [ 1 ]; do
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MdBESuQVi96Bxrb5YscJPWP3nkXxdG3Hzs.dedisut -t8
+sleep 2
+done
